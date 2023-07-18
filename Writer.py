8fe3835c6259bb0892db5eba14a85e45e1fdecfa
I-see Jaidee <wj100@wellesley.edu>
2023-07-18 09:10:04 -0500

    add ElementTree

--- Writer.py
+++ Writer.py
@@ -8,10 +8,26 @@ class Writer:
         # install in venv using
         # pip install dict2xml
         from dict2xml import dict2xml
+        import xml.etree.ElementTree as ET
+        def dict_to_xml(data, parent):
+            for key, value in data.items():
+                if isinstance(value, dict):
+                    element = ET.SubElement(parent, key)
+                    dict_to_xml(value, element)
+                else:
+                    element = ET.SubElement(parent, key)
+                    element.text = str(value)
+        root = ET.Element("ROOT")
+        dict_to_xml(result.fResultDict,root)
+        tree = ET.ElementTree(root)
+        ET.indent(tree, space="    ", level=0)
+        tree.write(outputXMLFile, encoding="utf-8", xml_declaration=True, method="xml")
+        print("XML saved to output.xml")
+        
 
+        #dict2xml
         xml = dict2xml(result.fResultDict, indent ="   ")
-
-        outputFile = open(outputXMLFile, "w")
+        outputFile = open(outputXMLFile+'2', "w")
         outputFile.write(xml)
         outputFile.close()
 
@@ -23,6 +39,7 @@ class Writer:
 
     def writeScoreToHTML(self, score, outputHTMLFile):
         print("Writing score to HTML")
-
+        
+                
 
     
